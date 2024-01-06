1dd896b89d1e4ca7df7597bc27faeb8618114910
proyecto3pslda <155788864+proyecto3pslda@users.noreply.github.com>
2024-01-06 15:40:20 -0600

    Create facebookLogin.js

---
+++ js/facebookLogin.js
@@ -0,0 +1,23 @@
+const facebookButton = document.querySelector('#facebookLogin');
+
+facebookButton.addEventListener('click', async e => {
+  e.preventDefault();
+
+  const provider = new FacebookAuthProvider();
+
+  try {
+    const credentials = await signInWithPopup(auth, provider)
+    console.log(credentials);
+    console.log("facebook sign in");
+    
+    // Close the login modal
+    const modal = bootstrap.Modal.getInstance(facebookButton.closest('.modal'));
+    modal.hide();
+
+    // show welcome message
+    showMessage("Welcome" + credentials.user.email);
+  } catch (error) {
+    console.log(error);
+  }
+
+})
